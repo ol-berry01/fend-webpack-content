3b1fae7ad17883c0957778f08d009b533532505a
ol-berry01 <info@littlemonsterdigital.co.uk>
2021-01-25 14:58:18 +0000

    Webpack Mode Update

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,22 +1,12 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-<<<<<<< Updated upstream
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
-module.exports = {
-    entry: './src/client/index.js',
-    mode: 'development',
-    devtool: 'source-map',
-    stats: 'verbose',
-=======
 
 module.exports = {
     mode: "development",
     devtool: "source-map",
     entry: './src/client/index.js',
     devtool: 'source-map',
->>>>>>> Stashed changes
     module: {
         rules: [
             {
@@ -30,18 +20,6 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-<<<<<<< Updated upstream
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-=======
->>>>>>> Stashed changes
         })
     ]
 }
